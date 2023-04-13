8f9fd8e011d22e1de11bbda473c6c5c6d957fce2
Vasishtadepu <80567051+Vasishtadepu@users.noreply.github.com>
2023-04-13 20:36:28 +0530

    Add files via upload

--- create_table.sql
+++ create_table.sql
@@ -5,8 +5,8 @@ CREATE TABLE IF NOT EXISTS `studentlogin`
     `username` varchar(50) NOT NULL,
     `password` varchar(50) NOT NULL,
     `email` varchar(50) NOT NULL,
-    'rollno' varchar(50) NOT NULL,
-    'department' varchar(50) NOT NULL,
+    `rollno` varchar(50) NOT NULL,
+    `department` varchar(50) NOT NULL,
     PRIMARY KEY(id)
 
 );
@@ -26,6 +26,7 @@ CREATE TABLE IF NOT EXISTS `submittedforms`
     `id` int(11) NOT NULL AUTO_INCREMENT,
     `formtype` int(11) NOT NULL,
     `rollno` varchar(50) NOT NULL,
+    `status` varchar(50) NOT NULL,
     PRIMARY KEY(id)
 
 );
@@ -36,18 +37,19 @@ CREATE TABLE IF NOT EXISTS `additionalcourseconversion`
     `course1` varchar(50) NOT NULL,
     `coursenumber1` varchar(50) NOT NULL,
     `credits1` int(11) NOT NULL,
-    'semester1' int(11) NOT NULL,
+    `semester1` int(11) NOT NULL,
     `course2` varchar(50) NOT NULL,
     `coursenumber2` varchar(50) NOT NULL,
     `credits2` int(11) NOT NULL,
-    'semester2' int(11) NOT NULL,
-    'approver1' varchar(50) NOT NULL,
-    'approver2' varchar(50) NOT NULL,
-    'approver3' varchar(50) NOT NULL,
-    'approver4' varchar(50) NOT NULL,
+    `semester2` int(11) NOT NULL,
+    `approver1` varchar(50) NOT NULL,
+    `approver2` varchar(50) NOT NULL,
+    `approver3` varchar(50) NOT NULL,
+    `approver4` varchar(50) NOT NULL,
+    `approvelevel` varchar(50) NOT NULL,
     PRIMARY KEY(id)
 
 );
 
-INSERT INTO studentlogin(username,password,email) VALUES ('Manaswini','2312','cs20btech11035@iith.ac.in');
+INSERT INTO studentlogin(username,password,email,rollno,department) VALUES ('Manaswini','2312','cs20btech11035@iith.ac.in','CS20BTECH11035','CSE');
 INSERT INTO adminlogin(username,password,email) VALUES ('Vasisht','2711','cs20btech11002@iith.ac.in');
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 from flask_mysqldb import MySQL
+from flask_mail import Mail,Message
 import MySQLdb.cursors
 import re
-
+import smtplib
+import mysql.connector
+ 
  
 app = Flask(__name__)
  
@@ -11,9 +14,25 @@ app.secret_key = 'your secret key'
  
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = '2312'
-app.config['MYSQL_DB'] = 'geeklogin'
-mysql = MySQL(app)
+app.config['MYSQL_PASSWORD'] = 'Vasisht@27'
+app.config['MYSQL_DB'] = 'SWE'
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'cs20btech11035@iith.ac.in'
+app.config['MAIL_PASSWORD'] = 'fgbboncngfheozws'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+mail = Mail(app)
+mysql2 = MySQL(app)
+
+sender_mail = "teamswe30@gmail.com"
+sender_password = "txaxmstjnwhwofpi"
+
+forms_dictionary = {
+    '1' : "Addtional Course Conversion",
+    '2' : "Assessment Commitee form"
+}
+
 
 @app.route('/')
 @app.route('/login', methods =['GET', 'POST'])
@@ -23,7 +42,7 @@ def login():
         username = request.form['username']
         password = request.form['password']
         user=request.form['user']
-        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor = mysql2.connection.cursor(MySQLdb.cursors.DictCursor)
         if user=='Student':
             cursor.execute('SELECT * FROM studentlogin WHERE username = % s AND password = % s', (username, password, ))
             user_account = cursor.fetchone()
@@ -54,23 +73,92 @@ def logout():
 def formlist():
     return render_template('formslist.html')
 
+@app.route('/create_forms',methods = ['GET','POST'])
+def create_forms():
+    if request.method == 'POST' and 'Form_type' in request.form:
+        session['formname'] = forms_dictionary[request.form['Form_type']]
+    return render_template('additionalcourseconversion.html')
+
+@app.route('/process_form',methods = ['GET','POST'])
+def process_form():
+    msg = Message(
+        request.form['subject'],
+        sender = sender_mail,
+        recipients= [request.form['email']]
+    )
+    msg.body = request.form['message']
+    mail.send(msg)
+    return 'sent'
+
 
 class AdditionalCourseConversionForm:
-
-    def __init__(self,name,roll_no,):
-
-    
     def create_instance():
+        return render_template('additionalcourseconversion.html')
+    def save_instance(dict):
+        mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="Vasisht@27",
+        database="SWE"
+        )
+        query2 = 'INSERT INTO submittedforms (formtype,rollno,status) VALUES (%s,%s,%s)'
+        values2 = (
+            dict['Form_type'],
+            dict['RollNo'],
+            0
+        )
+        mycursor = mydb.cursor()
+        mycursor.execute(query2,values2)
+        mydb.commit()
+        query3 = 'SELECT * from submittedforms where rollno = %s and formtype = %s'
+        values3 = (dict['RollNo'],dict['Form_type'])
+        mycursor.execute(query3,values3)
+        records = mycursor.fetchall()
+        row = records[-1]
+        form_id = row[0]
+        query1 = 'INSERT INTO additionalcourseconversion VALUES (%s, %s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)'
+        # cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        values1 = (
+            form_id,
+            dict['CourseName1'],
+            dict['CourseNumber1'],
+            int(dict['Credits1']),
+            int(dict['Semester1']),
+            dict['CourseName2'],
+            dict['CourseNumber2'],
+            int(dict['Credits2']),
+            int(dict['Semester2']),
+            dict['Guidemail'],
+            dict['HoDmail'],
+            dict['Registrarmail'],
+            dict['Deanmail'],
+            0
+        )
+        mycursor.execute(query1, values1)
+        mydb.commit()
+        return "Form submitted"
 
+def Factory(forms = '1'):
+    forms_dict = {'1' : AdditionalCourseConversionForm}
+    return forms_dict[forms]
 
-@app.route('\create_instance',methods=['GET','POST'])
+@app.route('/create_instance',methods=['GET','POST'])
 def create_instance():
-    
-@app.route('\save_instance',methods=['GET','POST'])
+    if request.method == 'POST':
+        form_obj = Factory(request.form['Form_type'])
+    return form_obj.create_instance()   
+
+@app.route('/save_instance',methods = ['GET','POST'])
 def save_instance():
+    if request.method == "POST":
+        form_obj = Factory(request.form['Form_type'])
+    return form_obj.save_instance(request.form)
+
+
+
+
+
     
-
-
 
 
 
